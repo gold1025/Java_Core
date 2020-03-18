95c71ba2bf1adf565335e3afbd5b70103d688eee
Acer <Acer@LAPTOP-ERHLK6L1>
2020-03-18 17:52:31 +0200

    added first project to github

---
+++ lesson-01/src/ua/lviv/lgs/Application.java
@@ -0,0 +1,53 @@
+package ua.lviv.lgs;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		
+//		Завдання 1
+		
+//		boolean q = true;
+//		char w = 'a';
+//		short e = 10;
+//		long r = 100000;
+//		byte t = -12;
+//		int y = 3;
+//		double u = 1.2;
+//		float i = 5;
+//		System.out.println("Змінні різних типів даних - "+ q +", " + w + ", " + e + ", " + r + ", " + t + ", " + y + ", " + u + ", " + i +".");
+//		
+		
+//		Завдання 2 
+		
+//		System.out.print(" Short MAX = " + Short.MAX_VALUE + "; ");
+//		System.out.print(" Long MAX = " + Long.MAX_VALUE + "; ");
+//		System.out.print(" Byte MAX = " + Byte.MAX_VALUE + "; ");
+//		System.out.print(" Integer MAX = " + Integer.MAX_VALUE + "; ");
+//		System.out.print( "Double MAX = " + Double.MAX_VALUE + "; ");
+//		System.out.println(" Float MAX = " + Float.MAX_VALUE + "; ");
+//
+//		System.out.print(" Short MIN = " + Short.MIN_VALUE + "; ");
+//		System.out.print(" Long MIN = " + Long.MIN_VALUE + "; ");
+//		System.out.print(" Byte MIN = " + Byte.MIN_VALUE + "; ");
+//		System.out.print(" Integer MIN = " + Integer.MIN_VALUE + "; ");
+//		System.out.print(" Double MIN = " + Double.MIN_VALUE + "; ");
+//		System.out.print(" Float MIN = " + Float.MIN_VALUE + "; ");
+		
+		
+//		Завдання 3
+//		
+//		int [] arrey = {3,5,7,17,15,13,11,9,1,100};
+//		int max = arrey[0];
+//		int min = arrey[0];
+//		for (int i = 0; i<10; i++) {
+//			if ( max < arrey[i])
+//				max = arrey[i];
+//		}
+//		for (int i = 0; i<10; i++) {
+//			if ( min > arrey[i])
+//				min = arrey[i];
+//		}
+//		System.out.println("MAX = " + max);
+//		System.out.println("MIN = " + min);
+	}
+}
